53c001a0c05563a8ec63ff33c95f104a1a9c2b97
Aaron McDaid <aaron.mcdaid@gmail.com>
2018-12-26 21:20:33 +0100

    TrackOneStream, to record mean and variance, and a test for it

--- bayesianAB/event_stream.py
+++ bayesianAB/event_stream.py
@@ -1,11 +1,31 @@
 from typeguard import typechecked
-from numpy.random import RandomState
+import numpy as np
 
 @typechecked
 def gen_normals(loc: float, scale: float, seed: int):
-    rng = RandomState()
+    rng = np.random.RandomState()
     rng.seed(seed)
     while True:
         xs = rng.normal(loc, scale, 1000)
         for x in xs:
             yield x
+
+
+class TrackOneStream:
+
+    def __init__(self, g):
+        self._g = g
+        self._n = 0
+        self._sum = 0.0
+        self._sum_squares = 0.0
+
+    def advance(self):
+        x = next(self._g)
+        self._n += 1
+        self._sum += x
+        self._sum_squares += x*x
+
+    def get_mean_and_sddev(self):
+        m = self._sum / self._n
+        v = self._sum_squares / self._n - m**2
+        return m, np.sqrt(v)
--- tests/bayesianAB/test_event_stream.py
+++ tests/bayesianAB/test_event_stream.py
@@ -1,4 +1,4 @@
-from bayesianAB.event_stream import gen_normals
+from bayesianAB.event_stream import gen_normals, TrackOneStream
 import itertools as it
 import numpy as np
 from pytest import approx
@@ -10,3 +10,12 @@ def test_gen_normals():
     assert np.std(x) == approx(2.944864624211224)
 
 
+def test_TrackOneStream():
+    for mean in [-3, 0, 5]:
+        for stdev in [0.1, 1, 10]:
+            tos = TrackOneStream(gen_normals(mean, stdev, 1337))
+            for x in range(100000):
+                tos.advance()
+            m, s = tos.get_mean_and_sddev()
+            assert 0.99 < s/stdev < 1.01
+            assert -0.01 < (mean - m) / stdev < 0.01
