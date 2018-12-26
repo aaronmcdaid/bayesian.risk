a96990706080913ed2ef237a986a132af6ef6358
Aaron McDaid <aaron.mcdaid@gmail.com>
2018-12-26 01:37:18 +0100

    risk.py s/standard_risk/fast_standard_risk/

--- bayesianAB/risk.py
+++ bayesianAB/risk.py
@@ -1,5 +1,5 @@
 from typeguard import typechecked
-from bayesianAB.core_standard_risk import standard_risk
+from bayesianAB.core_standard_risk import fast_standard_risk
 
 
 @typechecked
@@ -10,4 +10,4 @@ def risk(x: float, loc: float = 0, scale: float = 1) -> float:
         this returns
             mean(min(Y,x))
     """
-    return loc + scale * standard_risk((x - loc) / scale)
+    return loc + scale * fast_standard_risk((x - loc)/scale)
