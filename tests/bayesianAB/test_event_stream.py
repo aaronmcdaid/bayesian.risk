1d9c7a05badc38ca6faf7777cea9c6ff00f72410
Aaron McDaid <aaron.mcdaid@gmail.com>
2018-12-31 13:58:03 +0100

    Delete most/all of the old system

--- tests/bayesianAB/test_event_stream.py
+++ tests/bayesianAB/test_event_stream.py
@@ -1,5 +1,4 @@
-from bayesianAB.event_stream import gen_normals, TrackOneStream, ABtest, random_variants, \
-        one_column_per_variant, seeded_RandomStates, seeded_RandomState, random_standard_normals, \
+from bayesianAB.event_stream import random_variants, one_column_per_variant, seeded_RandomStates, seeded_RandomState, random_standard_normals, \
         simulate_many_draws_for_many_variants, generate_cumulative_dataframes, generate_cumulative_dataframes_with_extra_columns, \
         SimulationParams, simple_dataframe_with_all_stats
 import itertools as it
@@ -159,72 +158,3 @@ def test_simple_dataframe_with_all_stats__regret():
     df = simple_dataframe_with_all_stats(weights, means, stdevs, 'expected_gain <= {}'.format(REGRET_THRESHOLD_TO_WAIT_FOR))
     assert df['expected_gain'].iloc[-1] <= REGRET_THRESHOLD_TO_WAIT_FOR
     assert df['expected_gain'].iloc[-2] >  REGRET_THRESHOLD_TO_WAIT_FOR
-
-
-def test_gen_normals():
-    g = gen_normals(5, 3, 1337)
-    x = list(it.islice(g, 1000))
-    assert np.mean(x) == approx(4.9234279241459875)
-    assert np.std(x) == approx(2.944864624211224)
-
-
-def test_TrackOneStream():
-    for mean in [-3, 0, 5]:
-        for stdev in [0.1, 1, 10]:
-            tos = TrackOneStream(gen_normals(mean, stdev, 1337))
-            tos.advance(steps = 100000)
-            m, s = tos.get_mean_and_sddev()
-            assert 0.99 < s / stdev < 1.01
-            assert -0.01 < (mean - m) / stdev < 0.01
-
-
-def test_ABtest_weights():
-    # Generate with a 30/70 split, and check that is
-    # the observed split.
-    x = [
-        (3, gen_normals(0, 1, 1337)),
-        (7, gen_normals(0, 1, 1337)),
-    ]
-    ab = ABtest.from_list_of_pairs(x, 1337)
-    ab.advance(steps = 1000)
-    ns, _, _ = ab.get_ns_means_sddevs()
-    assert ns == [306, 694]
-
-
-def test_ABtest_means_and_sddevs():
-    # Generate with a few different means and stdevs,
-    # and verify the results are approximately as
-    # expected.
-    x = [
-        (3, gen_normals(0, 1, 1337)),
-        (7, gen_normals(3, 2, 1337)),
-        (7, gen_normals(8, 3, 1337)),
-    ]
-    ab = ABtest.from_list_of_pairs(x, 1337)
-    ab.advance(steps = 1000)
-    _, means, stdevs = ab.get_ns_means_sddevs()
-    assert means == approx([0, 3, 8], abs=0.1)
-    assert stdevs == approx([1, 2, 3], abs=0.1)
-
-
-def test_ABtest_at_least_two():
-    x = [
-        (3, gen_normals(0, 1, 1337)),
-        (7, gen_normals(0, 1, 1337)),
-    ]
-    ab = ABtest.from_list_of_pairs(x, 1337)
-    # Even though 'advance' isn't called here, we expect that
-    # each variant has two observations in it already
-    ns, _, _ = ab.get_ns_means_sddevs()
-    assert ns == [2, 2]
-
-
-def test_ABtest_simple():
-    # Generate with a few different means and stdevs,
-    # and verify the results are approximately as
-    # expected.
-    ab = ABtest.from_simple_args(true_diff=3, sd=2, weight=0.3, seed=1337, steps=1000)
-    ns, means, stdevs = ab.get_ns_means_sddevs()
-    assert ns == [306, 694]
-    assert means[1] - means[0] == approx(3, abs=0.1)
-    assert stdevs == approx([2, 2], abs=0.1)
