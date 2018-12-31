264b7c655a87779c5904a4ab19dd424f6d5a0e78
Aaron McDaid <aaron.mcdaid@gmail.com>
2018-12-31 16:27:20 +0100

    Switch 'SimulationParamsForOneChunk' back to the 'namedtuple' trick, in order to get == for free

--- bayesianAB/event_stream.py
+++ bayesianAB/event_stream.py
@@ -44,18 +44,11 @@ class SimulationParams:
         self.seeds = seeds
 
 
-class SimulationParamsForOneChunk:
+class SimulationParamsForOneChunk(namedtuple('SimulationParamsForOneChunk', 'n M weights means stdevs')):
     """ This is an 'internal' class, which describes there bare minimum
     needed to compute one 'chunk' of observations. It doesn't know anything
     about stopping conditions though."""
-
-    @typechecked
-    def __init__(self, n: int, M: int, weights: List[float], means: List[float], stdevs: List[float]):
-        self.n = n
-        self.M = M
-        self.weights = weights
-        self.means = means
-        self.stdevs = stdevs
+    pass
 
 
 @typechecked
