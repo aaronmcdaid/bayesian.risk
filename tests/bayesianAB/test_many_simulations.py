14703927d3b1b2af47d171065b2c9b6937d39f22
Aaron McDaid <aaron.mcdaid@gmail.com>
2019-01-06 13:01:56 +0100

    improve the test a little

--- tests/bayesianAB/test_many_simulations.py
+++ tests/bayesianAB/test_many_simulations.py
@@ -1,10 +1,24 @@
 from bayesianAB.many_simulations import many_simulations_to_the_stopping_condition
-def test_many_simulations_to_the_stopping_condition():
-    df = many_simulations_to_the_stopping_condition(10, seed = 1337)(
-            min_sample_size = 100,
-            stopping_condition = 'EL > -0.01',
+def test_many_simulations_to_the_stopping_condition__seeded():
+    RUNS = 100
+    l = lambda seed: many_simulations_to_the_stopping_condition(RUNS, seed = seed)(
+            min_sample_size = 10,
+            stopping_condition = 'total_sample_size >= 25',
             means = [7, 7],
             stdevs = [1,1],
             weights = [0.5, 0.5],
             )
-    print(df.groupby('run').tail(1))
+    df1 = l(1337)
+    df2 = l(1337)
+    df3 = l(None)
+    df4 = l(None)
+    EL1 = df1.groupby('run').tail(1).EL.values
+    EL2 = df2.groupby('run').tail(1).EL.values
+    EL3 = df3.groupby('run').tail(1).EL.values
+    EL4 = df4.groupby('run').tail(1).EL.values
+
+    # The first two are seeded the same, and hence should have identical results
+    assert (EL1 == EL2).all()
+    # The second two are unseeded, and will be different from each other
+    # (unless we're extremely unlucky)
+    assert not (EL3 == EL4).all()
