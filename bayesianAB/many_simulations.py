a10d120bbbd55b56854289225d88ade8dad66898
Aaron McDaid <aaron.mcdaid@gmail.com>
2019-01-06 12:46:01 +0100

    curried many_simulations_to_the_stopping_condition

--- bayesianAB/many_simulations.py
+++ bayesianAB/many_simulations.py
@@ -1,25 +1,44 @@
+from typeguard import typechecked, Optional
 from bayesianAB.event_stream import one_simulation_until_stopping_condition
 import pandas as pd
 
-def many_simulations_to_the_stopping_condition(runs, stopping_condition, means, stdevs, weights):
+@typechecked
+def _one_simulation(run: int, trace: bool, **kw):
+    """
+        To save a little memory, we return only four columns from the simulation:
+         - EL
+         - EG
+         - total_sample_size
+         - 'run' (this is just the 'run' parameter to this function)
+    """
+    if trace: print(run, end=' ')
+    sim = one_simulation_until_stopping_condition(**kw)
+    data_to_keep = sim[['total_sample_size', 'EL', 'EG']].copy()
+    data_to_keep['run'] = run
+    return data_to_keep
 
-    def _one_simulation(run):
-        print(run, end=' ')
-        sim = one_simulation_until_stopping_condition(
-            weights = weights,
-            # the absolute value of the means doesn't matter, just the difference between them
-            means = means,
-            stdevs = stdevs,
-            stopping_condition = stopping_condition,
-            min_sample_size = 100,
-        )
-        data_to_keep = sim[['total_sample_size', 'EL', 'EG']].copy()
-        data_to_keep['run'] = run
-        print(data_to_keep.head())
-        print(data_to_keep.tail())
-        print(stopping_condition)
-        return data_to_keep
+@typechecked
+def many_simulations_to_the_stopping_condition(runs: int, trace: bool = False, seed: Optional[int] = None):
+    """
+    Note that this is a 'curried' function. You call it like this:
+        many_simulations_to_the_stopping_condition(3)(..other args...)
+    where '3' is the number of runs. To get tracing for debugging purposes:
+        many_simulations_to_the_stopping_condition(3, trace=True)(..other args...)
 
-    many_runs = [_one_simulation(run) for run in range(runs)]
-    print()
-    return pd.concat(many_runs).reset_index(drop=True)
+    - 'run' is the number of simulations runs to do.
+    - 'trace' prints the run number between each simulation.
+    - The '..other args..' are passed directly to 'one_simulation_until_stopping_condition'.
+    - seed: If not None, then pass a different seed to each run, deterministically based
+            on 'seed'
+    """
+    def _curried(**kw):
+        many_runs = [_one_simulation(
+                        run = run,
+                        trace = trace,
+                        seeds = None if seed is None else (seed + 2*run, seed + 2*run +1),
+                        **kw,
+                        )
+                    for run in range(runs)]
+        if trace: print()
+        return pd.concat(many_runs).reset_index(drop=True)
+    return _curried
--- tests/bayesianAB/test_many_simulations.py
+++ tests/bayesianAB/test_many_simulations.py
@@ -1,12 +1,10 @@
 from bayesianAB.many_simulations import many_simulations_to_the_stopping_condition
 def test_many_simulations_to_the_stopping_condition():
-    many_simulations_to_the_stopping_condition(
-            3,
-            stopping_condition = 'EL < 0.01',
+    df = many_simulations_to_the_stopping_condition(10, seed = 1337)(
+            min_sample_size = 100,
+            stopping_condition = 'EL > -0.01',
             means = [7, 7],
             stdevs = [1,1],
-            weights = [0.5, 0.5]
+            weights = [0.5, 0.5],
             )
-
-
-
+    print(df.groupby('run').tail(1))
